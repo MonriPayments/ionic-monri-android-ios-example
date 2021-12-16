caf788ea39fc2843168c6106e8b32cae3a2fd0de
adnanomerovic <1812992171632>
2021-12-16 11:10:05 +0100

    Added example for saved card

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,7 +15,7 @@ export class HomePage {
   constructor(private httpClient: HttpClient) {
   }
 
-  monriPluginClickEvent($event: MouseEvent) {
+  monriPluginNewPayment($event: MouseEvent) {
     MonriHelper.createPaymentSession().then(//custom plugin for this
       session => {
         console.log('client_secret: ', session.client_secret);
@@ -31,17 +31,100 @@ export class HomePage {
                 pan: '4111 1111 1111 1111',
                 cvv: '123',
                 expiryMonth: 12,
-                expiryYear: 2023,
+                expiryYear: 2032,
                 saveCard: true
               },
               transaction: {
-                email: 'jasmin.suljich@gmail.com',
-                orderInfo: 'React native bridge???',
+                email: 'ionic.monri@gmail.com',
+                orderInfo: 'Ionic monri order info',
                 phone: '061123213',
                 city: 'Sarajevo',
                 country: 'BA',
                 address: 'Laticka',
-                fullName: 'Jasmin Suljic',
+                fullName: 'Ionic Monri Example',
+                zip: '71210',
+              },
+            }
+          }
+        );
+      }
+    ).then(r => JSON.stringify(r))
+      .then(result => {
+        this.paragraphElement.nativeElement.innerText = 'result: ' + result;
+      })
+      .catch(e => {
+        console.log('ERROR');
+        this.paragraphElement.nativeElement.innerText = 'error: ' + e;
+      });
+
+  }
+
+  monriPluginSavedCardPayment($event: MouseEvent) {
+    MonriHelper.createPaymentSession().then(//custom plugin for this
+      session => {
+        console.log('client_secret: ', session.client_secret);
+        this.paragraphElement.nativeElement.innerText = 'client_secret: ' + session.client_secret;
+        return IonicMonri.confirmPayment({
+            options: {
+              authenticityToken: '6a13d79bde8da9320e88923cb3472fb638619ccb',
+              developmentMode: true,
+            },
+            params: {
+              clientSecret: session.client_secret,
+              savedCard: {
+                panToken: 'd5719409d1b8eb92adae0feccd2964b805f93ae3936fdd9d8fc01a800d094584',
+                cvv: '123',//allow the user to enter cvv..
+              },
+              transaction: {
+                email: 'ionic.saved.card.no.3ds.monri@gmail.com',
+                orderInfo: 'Ionic monri order info saved card no 3DS',
+                phone: '061123213',
+                city: 'Sarajevo',
+                country: 'BA',
+                address: 'Laticka',
+                fullName: 'Ionic Monri Example',
+                zip: '71210',
+              },
+            }
+          }
+        );
+      }
+    ).then(r => JSON.stringify(r))
+      .then(result => {
+        this.paragraphElement.nativeElement.innerText = 'result: ' + result;
+      })
+      .catch(e => {
+        console.log('ERROR');
+        this.paragraphElement.nativeElement.innerText = 'error: ' + e;
+      });
+
+  }
+
+
+  monriPluginSavedCard3DSPayment($event: MouseEvent) {
+    MonriHelper.createPaymentSession().then(//custom plugin for this
+      session => {
+        console.log('client_secret: ', session.client_secret);
+        this.paragraphElement.nativeElement.innerText = 'client_secret: ' + session.client_secret;
+        return IonicMonri.confirmPayment({
+            options: {
+              authenticityToken: '6a13d79bde8da9320e88923cb3472fb638619ccb',
+              developmentMode: true,
+            },
+            params: {
+              clientSecret: session.client_secret,
+              savedCard: {
+                panToken: 'c32b3465be7278d239f68bb6d7623acf0530bf34574cf3b782754d281c76bd02',
+                cvv: '123',//allow the user to enter cvv..
+              },
+              transaction: {
+                email: 'ionic.saved.card.3ds.monri@gmail.com',
+                orderInfo: 'Ionic monri order info saved card 3DS',
+                phone: '061123213',
+                city: 'Sarajevo',
+                country: 'BA',
+                address: 'Laticka',
+                fullName: 'Ionic Monri Example',
                 zip: '71210',
               },
             }
